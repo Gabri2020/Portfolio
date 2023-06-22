33a0310e8a496021731202f37abbed10a98769dd
Luis Gabriel Aleman <gabriale223@gmail.com>
2023-06-22 01:12:05 -0300

    Ajuste el Javascript para reconocer si es tableta, ordenador al iniciar la pagina y hacer si el menu desplegable aparezca o no

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,34 @@
 //DETECTAR SI ES MOBIL O TABLET
 const photoskill = document.querySelectorAll('.img_skill');
-
+const cabeceraMaestra = document.querySelector('.container-encabezado');
+const cabecera = document.querySelectorAll('.content-border0');
+const divhamburger = document.querySelector('.contenedor-hamburguesa');
+const textoAzulCelular = document.querySelectorAll('.degradado-azul');
+const calcularDistElementosHorizontales = (objetoHorizontal) => {
+  let sumHorizontal = 25;
+  for (const elementoHorizontal of objetoHorizontal) {
+    sumHorizontal += elementoHorizontal.clientWidth;
+  }
+  return sumHorizontal;
+};
+let distElementosHorizontales = calcularDistElementosHorizontales(cabecera);
+function aparecerMenuDesplegable() {
+  cabeceraMaestra.classList.add('hamburguesa-derecha');
+  divhamburger.classList.add('hamburguesa-visible');
+  let m = cabecera.length;
+  for (var k = 0; k < m; k++) {
+    cabecera[k].classList.add('menu-invisible');
+  };
+  console.log("aparecerMenuDesplegable");
+};
+function desapareceMenuDesplegable() {
+  cabeceraMaestra.classList.remove('hamburguesa-derecha');
+  divhamburger.classList.remove('hamburguesa-visible');
+  let n = cabecera.length;
+  for (var l = 0; l < n; l++) {
+    cabecera[l].classList.remove('menu-invisible');
+  }
+}
 function detectar () {
   const ua = navigator.userAgent;
   if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
@@ -19,13 +47,25 @@ if (dispositivo == "mobile" || dispositivo == "tablet") {
     dataURL = dataURL.slice(-25);
     dataURL = dataURL.replace("white.svg",".svg");
     photoskill[i].src = dataURL;
-  }
+  };
+  let v = textoAzulCelular.length;
+  for (var r = 0; r < v; r++) {
+    textoAzulCelular[r].classList.add('sombra-mobile');
+  };
+
 };
+window.addEventListener("load", (e) => {
+  if (distElementosHorizontales > window.innerWidth) {
+    console.log(distElementosHorizontales, window.innerWidth);
+    aparecerMenuDesplegable();
+  } else {
+    desapareceMenuDesplegable();
+  };
+});
 //FIN DETECTAR SI ES MOBIL O TABLET
 
 //EFECTO SCROLL
 const animacion = document.querySelectorAll('.animado');
-const animacionheader = document.querySelectorAll('.content-border0')
 const animaciontop1 = document.getElementById('PERFIL');
 const animaciontop3 = document.getElementById('FRASE');
 const animaciontop2 = document.getElementById('SOY_YO');
@@ -35,10 +75,10 @@ const animaciontop2 = document.getElementById('SOY_YO');
 var i = 0;
 
 function mostrartop(){
-  var m = animacionheader.length;
+  var m = cabecera.length;
   for (var j = 0; j < m; j++) {
-    animacionheader[j].style.opacity = 1;
-    animacionheader[j].classList.add('mostrarabajo');
+    cabecera[j].style.opacity = 1;
+    cabecera[j].classList.add('mostrarabajo');
   }
   var scrolltop = document.documentElement.scrollTop;
   var scrollaltura = document.documentElement.clientHeight;
@@ -111,8 +151,10 @@ idiomaPlegableElemento.addEventListener("click", (e) => {
   changeDataLanguage(e);
 });
 // FIN DE CAMBIO DE IDIOMA
+
 // Look for .hamburger
 var hamburger = document.querySelector('.hamburger');
+
 var containerhamburger = document.querySelector('.columna-hamburguesa');
 var cuerpo = document.querySelector('body');
 var opacidad = document.querySelectorAll('section');
@@ -122,6 +164,27 @@ var truehamburger = false;
 var llave = false;
 var bandera = false;
 var truepresentacion = false;
+// const cabecera = document.querySelectorAll('.content-border0');
+
+// ACTIVAR .hamburger
+// window.addEventListener('resize', function() {
+//   if (distElementosHorizontales(cabecera) > window.innerWidth) {
+//     // console.log(distElementosHorizontales(cabecera), window.innerWidth);
+//     divhamburger.classList.add('hamburguesa-visible');
+//     let m = cabecera.length;
+//     for (var j = 0; j < m; j++) {
+//       cabecera[j].classList.add('menu-invisible');
+//     }
+//   } else {
+//     // console.log(distElementosHorizontales(cabecera), window.innerWidth);
+//     divhamburger.classList.remove('hamburguesa-visible');
+//     let m = cabecera.length;
+//     for (var j = 0; j < m; j++) {
+//       cabecera[j].classList.remove('menu-invisible');
+//     }
+//   }
+// });
+// FIN .hamburger
 // On click hamburger
 hamburger.addEventListener('click', function() {
   // Toggle class "is-active"
